feca5763b2f5dbae9d417742a1d2f644f0a0ef3a
Parshu108 <157161115+Parshu108@users.noreply.github.com>
2024-06-16 11:30:34 +0530

    Initialize first_branch

---
+++ Cpp.cpp
@@ -0,0 +1,22 @@
+// // Your First C++ Program
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout << "Enter a number:";
+//     cin >> n ;
+//     cout << "The squre of "<< n <<" is " <<n*n;
+//     return 0;
+// }
+//----20-01-2023
+/*#include <iostream>
+using namespace std;
+int main() {
+    int a=90;
+    cout << "address oa a =";
+    cout << &a;
+    cout << "\nvalue of a = ";
+    cout << a;
+    return 0;
+}*/
+//--------------
